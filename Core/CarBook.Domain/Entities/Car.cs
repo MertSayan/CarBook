732e73dace57815c5d10a91545f37f3b5fe89328
Mert Sayan <Mertrutenyumsayan@gmail.com>
2024-09-06 00:02:49 +0300

    Baslangic Entitiyleri eklendi

---
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; } 
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km {  get; set; }
+        public string Transmission {  get; set; } //enum yapıya çevrilebilir (vites türü)
+        public byte Seat { get; set; } // koltuk sayısı
+        public sbyte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
+    }
+}
