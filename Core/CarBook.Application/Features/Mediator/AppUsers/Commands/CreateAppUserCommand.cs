bde0c0cc1bcdbc37fd4b30d5d4b08f0621845116
Mert Sayan <Mertrutenyumsayan@gmail.com>
2024-10-25 10:24:42 +0300

    Register mediator ve enum yapısı bitti

---
+++ Core/CarBook.Application/Features/Mediator/AppUsers/Commands/CreateAppUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace CarBook.Application.Features.Mediator.AppUsers.Commands
+{
+	public class CreateAppUserCommand:IRequest
+	{
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+	}
+}
